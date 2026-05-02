4e8b0355ba994e1fbc2187cd8f85aae0a242a171
MarSol44 <marisolduran.eng@gmail.com>
2026-05-02 09:45:14 -0500

    Add files via upload

---
+++ examen-enunciado-MARISOLduran/examen-enunciado/pregunta2/javascript.js
@@ -0,0 +1,13 @@
+function mostrar(){
+    if (document.getElementById("1")){
+        document.getElementById("detalle").innerHTML=detalle
+    }
+    else if (document.getElementById("2")) {
+        document.getElementById("detalle").innerHTML=detalle2
+    }
+}
+
+
+function mostrar2() {
+    document.getElementById("detalle").innerHTML=detalle2
+}
